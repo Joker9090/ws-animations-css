4c276e5df31b40b5e51e4e0996f5ede1d8715ef3
Barto <juanmartin.cerruti@gmail.com>
2023-03-10 09:19:12 -0300

    v0

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
-export default function Home() {
+export default function SandBox() {
   return (
     <>
       <Head>
@@ -10,7 +9,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <main className='SandBox'>
+        <p>HOLA MUNDO</p>
+      </main>
     </>
   )
 }
