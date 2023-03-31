bec9f9a22fdf91ccd9fae6d2d62017dc257195c0
Barto <juanmartin.cerruti@gmail.com>
2023-03-31 10:01:14 -0300

    slider and animation sprite

--- components/SectionBackground.tsx
+++ components/SectionBackground.tsx
@@ -1,10 +1,42 @@
 import React from "react";
+import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
 
 export default function SectionBackground() {
   return (
     <section className='Background'>
       <BackgroundLogic />
+
+      <div className="container">
+        <Carrusel>
+          <div className="glide__slide BestieCardContainer">
+            <div className="BestieHolder top">
+              <BestieCard bestieName="eich" />
+              <BestieCard bestieName="jabra" />
+              <BestieCard bestieName="barti" />
+            </div>
+            <div className="BestieHolder bot">
+              <BestieCard bestieName="bluyark" />
+              <BestieCard bestieName="kalei" />
+            </div>
+          </div>
+          <div className="glide__slide BestieCardContainer">
+            <div className="BestieHolder top">
+              <BestieCard bestieName="kroku" />
+              <BestieCard bestieName="reny" />
+            </div>
+            <div className="BestieHolder bot">
+              <BestieCard bestieName="tekh" />
+            </div>
+          </div>
+        </Carrusel>
+      </div>
     </section>
+  )
+}
+
+export const BestieCard = ({ bestieName }: { bestieName: string }) => {
+  return (
+    <div className="BestieCard" style={{ backgroundImage: `url("/besties/${bestieName}.png");` }} />
   )
 }
 
@@ -25,7 +57,7 @@ export const BackgroundLogic = () => {
     }
     return () => {
       window.removeEventListener("resize", fixSize)
-    } 
+    }
   }, [LineRef, ParentRef])
 
   return (
@@ -33,6 +65,35 @@ export const BackgroundLogic = () => {
       <div className="Background-Pattern" ref={ParentRef} />
       <div className="Background-Progressive-Line" >
         <div className="Background-Progressive-Child" ref={LineRef} />
+      </div>
+    </div>
+  )
+}
+
+
+export const Carrusel = ({ children }: { children: JSX.Element[] }) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    console.log("asdasd")
+    if (ref.current) {
+      new Glide(
+        ref.current,
+        {
+
+        }
+      ).mount({ Controls, Breakpoints })
+    }
+  }, [ref])
+  return (
+    <div className="glide" ref={ref}>
+      <div className="glide__track" data-glide-el="track">
+        <ul className="glide__slides">
+          {children}
+        </ul>
+      </div>
+      <div className="glide__arrows" data-glide-el="controls">
+        <button className="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+        <button className="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
       </div>
     </div>
   )
